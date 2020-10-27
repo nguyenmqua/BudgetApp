acd326b7c927e8079195a364bee8129941fd1d29
Qua Nguyen <Nguyenmqua@gmail.com>
2020-10-27 11:05:27 -0600

    debugged port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
-require("./routes/html.js")(app);
+// require("./routes/html.js")(app);
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
